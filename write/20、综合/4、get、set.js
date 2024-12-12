a541fda553bbe16b8e074064cfd8892dd0f944e5
fflinkexin <fflinkexin@chinatelecom.cn>
2024-12-12 17:34:08 +0800

    dev

--- write/20、综合/4、get、set.js
+++ write/20、综合/4、get、set.js
@@ -4,7 +4,7 @@ function formatKeys(path) {
   return path;
 }
 
-function getValue(target, path, defaultValue) {
+function getValue(target, path) {
   const key = formatKeys(path);
   const keys = key.split(".").filter((_) => _ !== "");
 
@@ -16,10 +16,26 @@ function getValue(target, path, defaultValue) {
   return result;
 }
 
+function setValue(target, path, value) {
+  const key = formatKeys(path);
+  const keys = key.split(".").filter((_) => _ !== "");
+
+  let i = 0;
+  let result = target;
+  for (; i < keys.length - 1; i++) {
+    if (result[keys[i]] === undefined) {
+      result[keys[i]] = {};
+    }
+    result = result[keys[i]];
+  }
+  result[keys[i]] = value;
+}
+
 var object = { a: [{ b: { c: 3 } }] }; // path: 'a[0].b.c'
 var array = [{ a: { b: [1] } }]; // path: '[0].a.b[0]'
 
-console.log(getValue(object, "a[0].b.c", 0)); // 3
-console.log(getValue(array, "[0].a.b[0]", 12)); // 1
+console.log(getValue(object, "a[0].b.c")); // 3
+console.log(getValue(array, "[0].a.b[0]")); // 1
+console.log(setValue(array, "[0].a.b.c", 12)); // 1
 
-// TODO 实现set
+console.log(object, array);
