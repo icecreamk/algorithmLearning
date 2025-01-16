f7359f7f8e05d4cfaf17e47237c19c95485cbe93
kaixin <kkxx.ll@qq.com>
2025-01-16 14:17:22 +0800

    2

---
+++ code/48.js
@@ -0,0 +1,42 @@
+// 旋转图像
+var rotate = function (matrix) {
+  const n = matrix.length;
+  const matrix_new = new Array(n).fill(0).map(() => new Array(n).fill(0));
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      matrix_new[j][n - i - 1] = matrix[i][j];
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      matrix[i][j] = matrix_new[i][j];
+    }
+  }
+};
+
+var rotate = function (matrix) {
+  const n = matrix.length;
+  //水平中轴线翻转
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    for (let j = 0; j < n; j++) {
+      [matrix[i][j], matrix[n - i - 1][j]] = [
+        matrix[n - i - 1][j],
+        matrix[i][j],
+      ];
+    }
+  }
+  //主对角线翻转
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+};
+
+// 123
+// 456
+// 789
+
+// 321
+// 654
+// 987
