91f81a67a555fa1e2575b5e6bf1ee77e1433787a
kaixin <kkxx.ll@qq.com>
2025-02-20 15:47:27 +0800

    1

---
+++ code/链表(快慢指针写法偏多)/翻转.js
@@ -0,0 +1,27 @@
+// 翻转链表
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var trainningPlan = function (head) {
+    let prev = null
+    let curr = head
+
+    while (curr) {
+
+        const temp = curr.next
+        curr.next = prev
+
+        prev = curr
+        curr = temp
+    }
+
+    return prev
+};
